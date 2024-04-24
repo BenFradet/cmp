f8788690d8279cd7dd7a9d94f63c11a76aa51097
benfradet <benjamin.fradet@gmail.com>
2024-04-24 22:47:52 +0200

    server

---
+++ server/src/error.rs
@@ -0,0 +1,47 @@
+use std::convert::Infallible;
+
+use serde_derive::Serialize;
+use warp::{http::StatusCode, reject::{Reject, Rejection}, reply::Reply};
+
+#[derive(Serialize)]
+struct ErrorMessage {
+    code: u16,
+    message: String,
+}
+
+const MISSING_QUERY_PARAM_MSG: &'static str = "No \"q\" query parameter specified";
+
+#[derive(Debug)]
+pub struct MissingQueryParam;
+
+impl Reject for MissingQueryParam {}
+
+pub async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
+    let code;
+    let message;
+
+    if err.is_not_found() {
+        code = StatusCode::NOT_FOUND;
+        message = "NOT_FOUND";
+    } else if let Some(MissingQueryParam) = err.find() {
+        code = StatusCode::BAD_REQUEST;
+        message = MISSING_QUERY_PARAM_MSG;
+    } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
+        // We can handle a specific error, here METHOD_NOT_ALLOWED,
+        // and render it however we want
+        code = StatusCode::METHOD_NOT_ALLOWED;
+        message = "METHOD_NOT_ALLOWED";
+    } else {
+        // We should have expected this... Just log and say its a 500
+        eprintln!("unhandled rejection: {:?}", err);
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+        message = "UNHANDLED_REJECTION";
+    }
+
+    let json = warp::reply::json(&ErrorMessage {
+        code: code.as_u16(),
+        message: message.into(),
+    });
+
+    Ok(warp::reply::with_status(json, code))
+}
---
+++ server/src/main.rs
@@ -0,0 +1,79 @@
+use std::{collections::HashMap, convert::Infallible};
+
+use error::MissingQueryParam;
+use futures::stream::{self, StreamExt};
+use provider::Provider;
+use reqwest::Client;
+use response::Response;
+use warp::{reject::{self, Rejection}, reply::Reply, Filter};
+
+pub mod error;
+pub mod provider;
+pub mod response;
+
+const PARALLEL_REQUESTS: usize = 3;
+const QUERY_PARAM: &'static str = "q";
+
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> () {
+    let search = warp::get()
+        .and(warp::path!("api" / "v1" / "search"))
+        .and(extract_q())
+        .and_then(|search_term: String| search(search_term));
+
+    let hi = warp::path("hi").map(|| "Hello, World!");
+
+    let routes = search.or(hi).recover(error::handle_rejection);
+    println!("running at localhost:3030");
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn extract_q() -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
+    warp::query::<HashMap<String, String>>()
+        .and_then(|p: HashMap<String, String>| async move {
+            match p.get(QUERY_PARAM) {
+                Some(search_term) => Ok(search_term.to_owned()),
+                None => Err(reject::custom(MissingQueryParam)),
+            }
+        })
+}
+
+async fn search(search_term: String) -> Result<impl Reply, Infallible> {
+    let res = fetch(&search_term).await;
+    Ok(warp::reply::json(&Response { results: res }))
+}
+
+async fn fetch(search_term: &str) -> Vec<String> {
+    let client = Client::new();
+
+    let providers = vec![Provider::BIKE_DISCOUNT, Provider::ALLTRICKS, Provider::STARBIKE];
+    let providers_len = providers.len();
+
+    let results = stream::iter(providers)
+        .map(|p| {
+            // should be safe to clone since backed by an rc (to check)
+            let client = client.clone();
+            // corouting moves
+            let s = search_term.to_owned();
+            tokio::spawn(async move {
+                p.crawl(&client, &s).await.unwrap_or("not found".to_owned())
+            })
+        })
+        .buffer_unordered(PARALLEL_REQUESTS)
+        .take(providers_len)
+        .collect::<Vec<_>>()
+        .await;
+
+    results
+        .into_iter()
+        .map(|r| match r {
+            Ok(s) => Some(s),
+            Err(e) => {
+                println!("join error: {e}");
+                None
+            },
+        })
+        .flatten()
+        .collect()
+}
