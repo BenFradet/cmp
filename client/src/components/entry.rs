7b8d31ae4da6c2f7173e6733112fce215da1f6a9
benfradet <benjamin.fradet@gmail.com>
2024-04-27 23:19:05 +0200

    entry component

--- client/src/components/entry.rs
+++ client/src/components/entry.rs
@@ -8,8 +8,33 @@ pub struct EntryProps {
 
 #[function_component(Entry)]
 pub fn entry(props: &EntryProps) -> Html {
-    let price = &props.item.price;
+    let Item { name, provider, price, image_link, link, time } = &props.item;
     html! {
-        <div class="cell">{price}</div>
+        <div class="card">
+            <header class="card-header">
+                <p class="card-header-title">{provider}</p>
+                <button class="card-header-icon" aria-label="more options">
+                    <span class="icon">
+                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                </button>
+            </header>
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img src={image_link.clone()} alt="Product image" />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="content">
+                    <span class="heading has-text-grey">{format!("Price for {} on {}:", name, provider)}</span>
+                    <h3 class="mt-2 mb-0">{price}</h3>
+                    <p class="highlight number">{price}</p>
+                    <time>{time}</time>
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href={link.clone()} class="card-footer-item">{format!("Go to {}!", provider)}</a>
+            </footer>
+        </div>
     }
 }
--- domain/src/item.rs
+++ domain/src/item.rs
@@ -1,4 +1,9 @@
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Item {
+    pub name: String,
+    pub provider: String,
     pub price: String,
+    pub image_link: String,
+    pub link: String,
+    pub time: String,
 }
