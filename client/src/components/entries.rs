3d10cff6974c0a404d6079bbc68ff7ef47f736f3
benfradet <benjamin.fradet@gmail.com>
2024-05-08 21:48:10 +0200

    use_future_with

--- client/src/components/entries.rs
+++ client/src/components/entries.rs
@@ -1,9 +1,9 @@
 use web_sys::console;
 use yew::prelude::*;
+use yew::suspense::use_future_with;
 
 use crate::services::search::search;
 use crate::components::entry::Entry;
-use crate::components::progress_bar::ProgressBar;
 use crate::components::hero::Hero;
 
 #[derive(PartialEq, Properties, Clone)]
@@ -15,36 +15,28 @@ const INITIAL_SEARCH_TERM: &'static str = "Selle italia slr boost endurance";
 
 #[function_component(Entries)]
 pub fn entries(props: &EntriesProps) -> HtmlResult {
-    let results = use_state(|| None);
-
     let input = props.input.clone();
-    {
-        let input_clone = input.clone();
-        let results = results.clone();
+    let results = use_future_with(input, |i| async move {
+        search(i.as_deref()).await
+    })?;
 
-        use_effect_with(input, move |_| {
-            let input_clone_clone = input_clone.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                let res = search(input_clone_clone.as_deref()).await;
-                results.set(Some(res));
-            });
-            || {}
-        });
-    }
-
-    let result_html = match &(*results) {
-        None => html! { <ProgressBar /> },
-        Some(Ok(ref items)) =>
+    let result_html = match *results {
+        Ok(ref items) =>
             if items.is_empty() {
                 html! { "not found" }
             } else {
-                // find a way without clone
+                // todo: find a way without clone
                 let mut items = items.clone();
                 items.sort_by(|a, b| a.price.partial_cmp(&b.price).unwrap());
+
                 let list = items.iter().map(|item| html! {
                     <Entry item={item.clone()} />
                 }).collect::<Html>();
-                let txt = format!("Results for \"{}\":", props.input.clone().unwrap_or(AttrValue::Static(INITIAL_SEARCH_TERM)));
+
+                let result_text = props.input.clone()
+                    .unwrap_or(AttrValue::Static(INITIAL_SEARCH_TERM));
+                let txt = format!("Results for \"{}\":", result_text);
+
                 html! {
                     <div>
                         <Hero text={txt} />
@@ -54,7 +46,7 @@ pub fn entries(props: &EntriesProps) -> HtmlResult {
                     </div>
                 }
             },
-        Some(Err(ref failure)) => {
+        Err(ref failure) => {
             console::log_1(&format!("failure to receive response: {failure}").into());
             failure.to_string().into()
         },
